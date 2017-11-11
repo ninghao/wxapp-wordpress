b8b8615aeb347afc261f8ca75361ed8f34ba67e1
wanghao8080 <117663444@qq.com>
2017-11-11 11:59:59 +0800

    内容列表：获取内容列表数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,22 @@
 const app = getApp()
 
+const API_BASE = 'https://wp-dev.ninghao.net/wp-json'
+const API_ROUTE = 'wp/v2/posts'
+
 Page({
   data: {
+    entities: []
   },
   onLoad () {
+    wx.request({
+      url: `${ API_BASE }/${ API_ROUTE }`,
+      success: (response) => {
+        console.log(response)
+        const entities = response.data
+        this.setData({
+          entities
+        })
+      }
+    })
   }
 })
