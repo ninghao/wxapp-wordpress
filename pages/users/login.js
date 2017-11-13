54536fb4c89af4c0f36f6bf8ba2ad78d10940fb1
wanghao8080 <117663444@qq.com>
2017-11-13 15:36:04 +0800

    身份验证：身份验证失败的显示信息

--- pages/users/login.js
+++ pages/users/login.js
@@ -4,7 +4,9 @@ const API_ROUTE = 'jwt-auth/v1/token'
 Page({
   data: {
     username: '',
-    password: ''
+    password: '',
+    showMessage: false,
+    message: ''
   },
   onInputUsername (event) {
     this.setData({
@@ -28,6 +30,45 @@ Page({
       },
       success: (response) => {
         console.log(response)
+        const { data, statusCode } = response
+
+        if (data.hasOwnProperty('code')) {
+          switch (data.code) {
+            case '[jwt_auth] invalid_username':
+              this.setData({
+                message: '用户名不对'
+              })
+              break
+            case '[jwt_auth] incorrect_password':
+              this.setData({
+                message: '密码错了'
+              })
+              break
+            default:
+              this.setData({
+                message: '请检查用户名或密码'
+              })
+          }
+        }
+
+        switch (statusCode) {
+          case 403:
+            this.setData({
+              showMessage: true
+            })
+
+            setTimeout(() => {
+              this.setData({
+                showMessage: false
+              })
+            }, 3000)
+            break
+          case 200:
+            console.log(response.data)
+            break
+          default:
+            console.log(response)
+        }
       }
     })
   }
