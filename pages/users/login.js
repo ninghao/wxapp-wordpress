2e2c1a9c43cb9b73b3837b50eb1aed5e72505a75
wanghao8080 <117663444@qq.com>
2017-11-13 12:52:26 +0800

    身份验证：得到用户输入的用户名与密码

--- pages/users/login.js
+++ pages/users/login.js
@@ -1,3 +1,19 @@
 Page({
-  
+  data: {
+    username: '',
+    password: ''
+  },
+  onInputUsername (event) {
+    this.setData({
+      username: event.detail.value
+    })
+  },
+  onInputPassword (event) {
+    this.setData({
+      password: event.detail.value
+    })
+  },
+  onTapSubmitButton () {
+    console.log(this.data.username, this.data.password)
+  }
 })
