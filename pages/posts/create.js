4f57758f867cfe939ea42c3df877b20cde90b690
wanghao8080 <117663444@qq.com>
2017-11-14 13:39:33 +0800

    发布内容成功以后清空创作页面的显示

--- pages/posts/create.js
+++ pages/posts/create.js
@@ -1,6 +1,20 @@
+const app = getApp()
+
+const API_BASE = 'https://wp-dev.ninghao.net/wp-json'
+const API_ROUTE = 'wp/v2/posts'
+
 Page({
   data: {
-    entity: {}
+    entity: {},
+    jwt: {}
+  },
+  onShow () {
+    const { jwt } = app.globalData
+    this.setData({
+      jwt: {
+        ...jwt
+      }
+    })
   },
   onInputTitle (event) {
     this.setData({
@@ -19,5 +33,31 @@ Page({
   },
   onTapSubmitButton () {
     console.log(this.data.entity)
+
+    wx.request({
+      url: `${ API_BASE }/${ API_ROUTE }`,
+      method: 'POST',
+      header: {
+        'Authorization': `Bearer ${ this.data.jwt.token }`
+      },
+      data: {
+        ...this.data.entity
+      },
+      success: (response) => {
+        switch (response.statusCode) {
+          case 201:
+            this.setData({
+              entity: {}
+            })
+
+            wx.navigateTo({
+              url: `/pages/posts/show?id=${ response.data.id }`
+            })
+            break
+          default:
+            console.log(response)
+        }
+      }
+    })
   }
 })
