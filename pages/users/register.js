11add8ccd43692575c3bbba62cd064e4aa791d80
wanghao8080 <117663444@qq.com>
2017-11-28 15:20:26 +0800

    显示注册用户时发生的错误

--- pages/users/register.js
+++ pages/users/register.js
@@ -43,46 +43,44 @@ Page({
         console.log(response)
         const { data, statusCode } = response
 
-        // if (data.hasOwnProperty('code')) {
-        //   switch (data.code) {
-        //     case '[jwt_auth] invalid_username':
-        //       this.setData({
-        //         message: '用户名不对'
-        //       })
-        //       break
-        //     case '[jwt_auth] incorrect_password':
-        //       this.setData({
-        //         message: '密码错了'
-        //       })
-        //       break
-        //     default:
-        //       this.setData({
-        //         message: '请检查用户名或密码'
-        //       })
-        //   }
-        // }
-        //
-        // switch (statusCode) {
-        //   case 403:
-        //     this.setData({
-        //       showMessage: true
-        //     })
-        //
-        //     setTimeout(() => {
-        //       this.setData({
-        //         showMessage: false
-        //       })
-        //     }, 3000)
-        //     break
-        //   case 200:
-        //     setJWT(response.data)
-        //     wx.switchTab({
-        //       url: '/pages/users/show'
-        //     })
-        //     break
-        //   default:
-        //     console.log(response)
-        // }
+        if (data.hasOwnProperty('code')) {
+          this.setData({
+            message: data.message
+          })
+        }
+
+        switch (statusCode) {
+          case 500:
+            this.setData({
+              showMessage: true
+            })
+
+            setTimeout(() => {
+              this.setData({
+                showMessage: false
+              })
+            }, 3000)
+            break
+          case 400:
+            this.setData({
+              showMessage: true
+            })
+
+            setTimeout(() => {
+              this.setData({
+                showMessage: false
+              })
+            }, 3000)
+            break
+          case 200:
+            // setJWT(response.data)
+            // wx.switchTab({
+            //   url: '/pages/users/show'
+            // })
+            break
+          default:
+            console.log(response)
+        }
       }
     })
   }
