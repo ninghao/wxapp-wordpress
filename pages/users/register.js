0d55f3d90ab07f7831fb97f0abd51ea95eb5f453
wanghao8080 <117663444@qq.com>
2017-11-28 14:05:22 +0800

    注册新用户

--- pages/users/register.js
+++ pages/users/register.js
@@ -3,6 +3,7 @@ const { setJWT } = app
 
 const API_BASE = 'https://wp-dev.ninghao.net/wp-json'
 const API_ROUTE = 'jwt-auth/v1/token'
+const API_ROUTE_USER_REGISTER = 'users/v1/register'
 
 Page({
   data: {
@@ -28,59 +29,60 @@ Page({
     })
   },
   onTapSubmitButton () {
-    console.log(this.data.username, this.data.password)
+    const { username, password, email } = this.data
 
     wx.request({
-      url: `${ API_BASE }/${ API_ROUTE }`,
+      url: `${ API_BASE }/${ API_ROUTE_USER_REGISTER }`,
       method: 'POST',
       data: {
-        username: this.data.username,
-        password: this.data.password
+        username,
+        password,
+        email
       },
       success: (response) => {
         console.log(response)
         const { data, statusCode } = response
 
-        if (data.hasOwnProperty('code')) {
-          switch (data.code) {
-            case '[jwt_auth] invalid_username':
-              this.setData({
-                message: '用户名不对'
-              })
-              break
-            case '[jwt_auth] incorrect_password':
-              this.setData({
-                message: '密码错了'
-              })
-              break
-            default:
-              this.setData({
-                message: '请检查用户名或密码'
-              })
-          }
-        }
-
-        switch (statusCode) {
-          case 403:
-            this.setData({
-              showMessage: true
-            })
-
-            setTimeout(() => {
-              this.setData({
-                showMessage: false
-              })
-            }, 3000)
-            break
-          case 200:
-            setJWT(response.data)
-            wx.switchTab({
-              url: '/pages/users/show'
-            })
-            break
-          default:
-            console.log(response)
-        }
+        // if (data.hasOwnProperty('code')) {
+        //   switch (data.code) {
+        //     case '[jwt_auth] invalid_username':
+        //       this.setData({
+        //         message: '用户名不对'
+        //       })
+        //       break
+        //     case '[jwt_auth] incorrect_password':
+        //       this.setData({
+        //         message: '密码错了'
+        //       })
+        //       break
+        //     default:
+        //       this.setData({
+        //         message: '请检查用户名或密码'
+        //       })
+        //   }
+        // }
+        //
+        // switch (statusCode) {
+        //   case 403:
+        //     this.setData({
+        //       showMessage: true
+        //     })
+        //
+        //     setTimeout(() => {
+        //       this.setData({
+        //         showMessage: false
+        //       })
+        //     }, 3000)
+        //     break
+        //   case 200:
+        //     setJWT(response.data)
+        //     wx.switchTab({
+        //       url: '/pages/users/show'
+        //     })
+        //     break
+        //   default:
+        //     console.log(response)
+        // }
       }
     })
   }
